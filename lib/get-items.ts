67e8b13684b3e20dc7f20d83ed952160fc38b8c2
h.miyazaki <aqla114@yahoo.co.jp>
2021-02-25 09:56:20 +0900

    GetItems で weight を渡すように修正 & id で sort されるようにした

--- lib/get-items.ts
+++ lib/get-items.ts
@@ -4,9 +4,15 @@ import { getRandomValues, getSumOfDice } from './dice-roll';
 export function getItems(itemPool: Recipe[], diceCount: number): Recipe[] {
 	const itemCount = getSumOfDice(diceCount);
 
-	const itemIndices: number[] = getRandomValues(itemCount, itemPool.length);
+	const itemIndices: number[] = getRandomValues(
+		itemCount,
+		itemPool.length,
+		itemPool.map((item) => item.dropRate / 5)
+	);
 
-	const items = itemIndices.map((idx) => itemPool[idx]);
+	const items = itemIndices
+		.map((idx) => itemPool[idx])
+		.sort((a, b) => a.id - b.id);
 
 	return items;
 }
